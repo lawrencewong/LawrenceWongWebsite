7d4bc9d278c122f1dec267809cbdca113a1e1331
Lawrence Wong <lawrencecywong@gmail.com>
2016-08-19 01:14:18 -0400

    Getting closer

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,4 +97,21 @@
 
 	});
 
+	jQuery('.menuLinks').each( function() {
+	    var $this = jQuery(this),
+	        target = this.hash;
+	    jQuery(this).click(function (e) {
+	        e.preventDefault();
+	        if( $this.length > 0 ) {
+	            if($this.attr('href') == '#' ) {
+	                // Do nothing
+	            } else {
+	               jQuery('html, body').animate({
+	                    scrollTop: (jQuery(target).offset().top) - 60
+	                }, 750);
+	            }
+	        }
+	    });
+	});
+
 })(jQuery);
